28f8cb7558572c0e001e6b401907104d03ada07d
Dustin Clifford <dclifford@sitesoftllc.net>
2016-07-18 00:35:11 -0400

    updating problem 1

--- src/main/java/com/bravolt/bravobox/route/RouteBuilder.java
+++ src/main/java/com/bravolt/bravobox/route/RouteBuilder.java
@@ -1,28 +1,8 @@
 package com.bravolt.bravobox.route;
 
-import org.apache.camel.model.dataformat.CsvDataFormat;
-
-import com.bravolt.bravobox.bean.MovieListResponseBuilder;
-import com.bravolt.bravobox.bean.ProcessMovieListRequest;
-import com.bravolt.bravobox.bean.ProcessMovieRequest;
-
 public class RouteBuilder extends org.apache.camel.builder.RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		CsvDataFormat csv = new CsvDataFormat();
-		
-		csv.setDelimiter(",");
-		
-		from("properties:inbound.movie.information")
-			.bean(ProcessMovieRequest.class)
-			.to("properties:outbound.movie.information");
-		
-		from("properties:inbound.movie.list")
-			.bean(ProcessMovieListRequest.class)
-			.bean(MovieListResponseBuilder.class)
-				.setProperty("inventoryDir").simple("properties:inventory.dir")
-				.setProperty("inventoryFileName").simple("properties:inventory.fileName");
-		
 	}
 }
--- src/test/java/com/bravolt/bravobox/BravoBoxTest.java
+++ src/test/java/com/bravolt/bravobox/BravoBoxTest.java
@@ -1,22 +1,10 @@
 package com.bravolt.bravobox;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.util.StringUtils;
-
-import com.bravolt.bravobox.model.movie.MovieListRequest;
-import com.bravolt.bravobox.model.movie.MovieListResponse;
-import com.bravolt.bravobox.model.movie.MovieRequest;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class BravoBoxTest extends CamelSpringTestSupport {
 	
@@ -31,89 +19,6 @@ public class BravoBoxTest extends CamelSpringTestSupport {
 
 	@After
 	public void tearDown() {
-	}
-	
-	@Test
-	public void testMovieInformation() {
-		Gson gson = new Gson();
-		MovieRequest request = new MovieRequest();
-		String requestJson;
-		String responseJson;
-		
-		request.setImdbMovie("tt0086567");
-		requestJson = gson.toJson(request);
-	
-		responseJson = template.requestBody(ENDPOINT_INFORMATION, requestJson, String.class);
-		
-		JsonParser parser = new JsonParser();
-		JsonObject obj = parser.parse(responseJson).getAsJsonObject();
-		
-		assertFalse(StringUtils.isEmpty(responseJson));
-	}
-	
-	@Test
-	public void testMovieListTemplate_All() {
-		String responseJson;
-		Gson gson = new Gson();
-		MovieListRequest request = new MovieListRequest();
-		String requestJson;
-		MovieListResponse[] responses = new MovieListResponse[1];
-		
-		requestJson = gson.toJson(request);
-		
-		responseJson = template.requestBody(ENDPOINT_LIST, requestJson, String.class);
-		responses = gson.fromJson(responseJson, responses.getClass());
-	
-		assertFalse(StringUtils.isEmpty(responseJson));
-		assertEquals(12, responses.length);
-	}
-	
-	@Test
-	public void testMovieListTemplate_Thriller() {
-		String responseJson;
-		Gson gson = new Gson();
-		MovieListRequest request = new MovieListRequest();
-		String requestJson;
-		MovieListResponse[] responses = new MovieListResponse[1];
-		String type = "Thriller";
-		
-		request.setType(type);
-		requestJson = gson.toJson(request);
-		
-		responseJson = template.requestBody(ENDPOINT_LIST, requestJson, String.class);
-		responses = gson.fromJson(responseJson, responses.getClass());
-	
-		assertFalse(StringUtils.isEmpty(responseJson));
-		for(MovieListResponse response : responses) {
-			List<String> types = Arrays.asList(response.getType().split(";"));
-		
-			assertTrue(types.contains(type));
-		}
-		assertEquals(4, responses.length);
-	}
-
-	@Test
-	public void testMovieListTemplate_Drama() {
-		String responseJson;
-		Gson gson = new Gson();
-		MovieListRequest request = new MovieListRequest();
-		String requestJson;
-		MovieListResponse[] responses = new MovieListResponse[1];
-		String type = "Drama";
-		
-		request.setType(type);
-		requestJson = gson.toJson(request);
-		
-		responseJson = template.requestBody(ENDPOINT_LIST, requestJson, String.class);
-		responses = gson.fromJson(responseJson, responses.getClass());
-	
-		assertFalse(StringUtils.isEmpty(responseJson));
-		for(MovieListResponse response : responses) {
-			List<String> types = Arrays.asList(response.getType().split(";"));
-		
-			assertTrue(types.contains(type));
-		}
-		assertEquals(5, responses.length);
 	}
 	
 	@Override
